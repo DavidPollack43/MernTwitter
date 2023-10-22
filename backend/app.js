bcf55de092071359a907c73df75ecdcc35a49881
David <2018dpollack@gmail.com>
2023-10-22 15:56:19 -0700

    Removed unnessesary lines of code

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,10 @@
 // backend/app.js
 
 const express = require("express");
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
+
 const usersRouter = require('./routes/api/users'); // update the import file path
 const tweetsRouter = require('./routes/api/tweets');
 
@@ -15,10 +14,8 @@ app.use(logger('dev')); // log request components (URL/method) to terminal
 app.use(express.json()); // parse JSON request body
 app.use(express.urlencoded({ extended: false })); // parse urlencoded request body
 app.use(cookieParser()); // parse cookies as an object on req.cookies
-app.use(express.static(path.join(__dirname, 'public'))); // serve the static files in the public folder
 
 // Attach Express routers
-app.use('/', indexRouter);
 app.use('/api/users', usersRouter); // update the path
 app.use('/api/tweets', tweetsRouter);
 
